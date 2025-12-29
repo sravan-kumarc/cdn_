a99670007c2fafd73e89ec91489d6a21085400e1
sravan-kumarc <sravankumarchagalamarri@gmail.com>
2025-12-29 12:33:35 +0000

    aws_cdn

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+set -e
+
+# Ensure correct ownership before npm install
+chown -R ec2-user:ec2-user /home/ec2-user/snake_Game || true
+
+# Stop any running node process
 pkill node || true
